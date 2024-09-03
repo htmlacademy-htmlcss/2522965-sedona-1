0e8434cc68628b0f3f02640268b89f5902dc6923
Valerian <mr.onixel@gmail.com>
2024-09-03 05:54:11 +0500

    Вводит экспериментальный скрипт
    Касательно счетчика детей и взрослых в модальном окне

--- js/script.js
+++ js/script.js
@@ -9,3 +9,33 @@ registrationButton.onclick = function() {
 closeModalButton.onclick = function() {
   modal.classList.remove("visible");
 };
+
+const counters = document.querySelectorAll('[data-counter]');
+
+if (counters) {
+  counters.forEach(container => {
+    container.addEventListener('click', e => {
+      const target = e.target;
+
+      if (target.closest('.counter__button')) {
+        let value = parseInt(target.closest('.container').querySelector('input').value);
+
+        if (target.classList.contains('plus')) {
+          value++;
+        } else {
+          --value;
+        }
+
+        if (value <= 0) {
+          value = 0;
+        }
+
+        if (value >= 10) {
+          value = 10;
+        }
+
+        target.closest('.container').querySelector('input').value = value;
+      }
+    })
+  })
+}
