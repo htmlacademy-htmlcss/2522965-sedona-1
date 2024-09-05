771f9f580efdd503c2afe63d897f16703a3562e6
Valerian <mr.onixel@gmail.com>
2024-09-06 03:26:00 +0500

    Вносит правки
    Изменяет js код для инпута количества гостей в модальном окне, выносит свг спрайты вниз, вносит визуальные незначительные правки

--- js/script.js
+++ js/script.js
@@ -10,15 +10,56 @@ closeModalButton.onclick = function() {
   modal.classList.remove("visible");
 };
 
-const counters = document.querySelectorAll('[data-counter]');
+const decrement = document.querySelector('#decrement-adults');
+const increment = document.querySelector("#increment-adults");
+const adultsNumber = document.querySelector("#adults-number");
+const decrementChilds = document.querySelector("#decrement-childs");
+const incrementChilds = document.querySelector("#increment-childs");
+const childsNumber = document.querySelector("#childs-number");
+
+decrement.addEventListener("click", () => {
+  if (adultsNumber.value <= 0) {
+    adultsNumber.value = 0;
+  } else {
+    adultsNumber.value = parseInt(adultsNumber.value) - 1;
+  }
+});
+
+increment.addEventListener("click", () => {
+  if (adultsNumber.value >= 10) {
+    adultsNumber.value = 10;
+  } else {
+    adultsNumber.value = parseInt(adultsNumber.value) + 1;
+  }
+});
+
+decrementChilds.addEventListener("click", () => {
+  if (childsNumber.value <= 0) {
+    childsNumber.value = 0;
+  } else {
+    childsNumber.value = parseInt(childsNumber.value) - 1;
+  }
+});
+
+incrementChilds.addEventListener('click', () => {
+  if (childsNumber.value >= 10) {
+    childsNumber.value = 10;
+  } else {
+    childsNumber.value = parseInt(childsNumber.value) + 1;
+  }
+});
+
+
+/*
+const counters = document.querySelectorAll('.container-counter');
 
 if (counters) {
   counters.forEach(container => {
     container.addEventListener('click', e => {
       const target = e.target;
 
-      if (target.closest('.counter__button')) {
-        let value = parseInt(target.closest('.container').querySelector('input').value);
+      if (target.closest('.counter-button')) {
+        let value = parseInt(target.closest('.container-counter').querySelector('input').value);
 
         if (target.classList.contains('plus')) {
           value++;
@@ -34,8 +75,9 @@ if (counters) {
           value = 20;
         }
 
-        target.closest('.container').querySelector('input').value = value;
+        target.closest('.container-counter').querySelector('input').value = value;
       }
     })
   })
 }
+*/
--- js/slider.js
+++ js/slider.js
@@ -1,18 +1,16 @@
-const rangeInput = document.querySelectorAll(".range-input input"),
-priceInput = document.querySelectorAll(".form-text-wrapper input"),
-progress = document.querySelector(".slider .progress");
+const rangeInput = document.querySelectorAll('.range-input input');
+const priceInput = document.querySelectorAll('.form-text-wrapper input');
+const progress = document.querySelector('.slider .progress');
 
 let PRICEGAP = 1000;
 
 rangeInput.forEach((input) => {
-  input.addEventListener("input", (e) => {
-    // getting two ranges value and parsing them to number
+  input.addEventListener('input', (e) => {
     let minVal = parseInt(rangeInput[0].value),
       maxVal = parseInt(rangeInput[1].value);
 
     if (maxVal - minVal < PRICEGAP) {
-      if (e.target.className === "range-min") {
-        //if active slider is min slider
+      if (e.target.className === 'range-min') {
         rangeInput[0].value = maxVal - PRICEGAP;
       } else {
         rangeInput[1].value = minVal + PRICEGAP;
@@ -20,8 +18,14 @@ rangeInput.forEach((input) => {
     } else {
       priceInput[0].value = minVal;
       priceInput[1].value = maxVal;
-      progress.style.left = (minVal / rangeInput[0].max) * 100 + "%";
-      progress.style.right = 100 - (maxVal / rangeInput[1].max) * 100 + "%";
+      progress.style.left = (minVal / rangeInput[0].max) * 100 + '%';
+      progress.style.right = 100 - (maxVal / rangeInput[1].max) * 100 + '%';
     }
   });
 });
+
+
+document.querySelector('#reset-btn').addEventListener('click', () => {
+  progress.style.left = 0 + '%';
+  progress.style.right = 25 + '%';
+})
